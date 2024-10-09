440534aa83fbdec4596370f5e1ac6a8cd52bdcd4
Mapetr <info@mapetr.moe>
2024-10-09 20:47:37 +0200

    add react random quote fetching

--- quotesapi.client/src/App.tsx
+++ quotesapi.client/src/App.tsx
@@ -1,56 +1,21 @@
-import { useEffect, useState } from 'react';
 import './App.css';
+import RandomQuote from "./components/RandomQuote.tsx";
 
-interface Forecast {
-    date: string;
-    temperatureC: number;
-    temperatureF: number;
-    summary: string;
+export interface Quote {
+  quoteId: number;
+  text: string;
+  created: string;
+  userId: string;
 }
 
-function App() {
-    const [forecasts, setForecasts] = useState<Forecast[]>();
-
-    useEffect(() => {
-        populateWeatherData();
-    }, []);
-
-    const contents = forecasts === undefined
-        ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-        : <table className="table table-striped" aria-labelledby="tabelLabel">
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Temp. (C)</th>
-                    <th>Temp. (F)</th>
-                    <th>Summary</th>
-                </tr>
-            </thead>
-            <tbody>
-                {forecasts.map(forecast =>
-                    <tr key={forecast.date}>
-                        <td>{forecast.date}</td>
-                        <td>{forecast.temperatureC}</td>
-                        <td>{forecast.temperatureF}</td>
-                        <td>{forecast.summary}</td>
-                    </tr>
-                )}
-            </tbody>
-        </table>;
-
-    return (
-        <div>
-            <h1 id="tabelLabel">Weather forecast</h1>
-            <p>This component demonstrates fetching data from the server.</p>
-            {contents}
-        </div>
-    );
-
-    async function populateWeatherData() {
-        const response = await fetch('weatherforecast');
-        const data = await response.json();
-        setForecasts(data);
-    }
+export interface Tag {
+  id: number;
+  text: string;
+  type: number;
 }
 
-export default App;
+export function App() {
+  return (
+    <RandomQuote />
+  )
+}
---
+++ quotesapi.client/src/components/RandomQuote.tsx
@@ -0,0 +1,22 @@
+﻿import {useEffect, useState} from "react";
+import {Quote} from "../App";
+
+export default function RandomQuote() {
+  const [quote, setQuote] = useState("Loading...");
+
+  useEffect(() => {
+    fetch("http://localhost:5146/api/Quotes/random")
+      .then((resp) => resp.json() as unknown as Quote)
+      .then((data) => {
+        setQuote(data.text);
+      })
+      .catch(err => {
+        console.error(err);
+        setQuote("Error");
+      })
+      }, []);
+
+    return (
+      <p>Random quote: {quote}</p>
+    )
+  }
