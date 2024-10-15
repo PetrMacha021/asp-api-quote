028b51f67ee4e19d183679558d14e18d7c0af629
Mapetr <info@mapetr.moe>
2024-10-15 19:49:28 +0200

    add scraper

---
+++ quotesapi.scraper/ApiService.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Http.Headers;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace quotesapi.scraper;
+
+public class ApiService
+{
+    private readonly HttpClient _httpClient;
+
+    public ApiService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task LoginAsync(string username, string password)
+    {
+        var loginData = new { email = username, password };
+        var content   = new StringContent(JsonConvert.SerializeObject(loginData), Encoding.UTF8, "application/json");
+
+        var response = await _httpClient.PostAsync("http://localhost:5146/login", content);
+        response.EnsureSuccessStatusCode();
+
+        var     responseData = await response.Content.ReadAsStringAsync();
+        dynamic data         = JsonConvert.DeserializeObject(responseData)!;
+
+        string tokenType   = data.tokenType;
+        string accessToken = data.accessToken;
+
+        _httpClient.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue(tokenType, accessToken);
+    }
+
+    public async Task AddQuoteAsync(string text) {
+        var quoteData = new { text };
+        var content   = new StringContent(JsonConvert.SerializeObject(quoteData), Encoding.UTF8, "application/json");
+
+        var response = await _httpClient.PostAsync("http://localhost:5146/api/Quotes", content);
+        response.EnsureSuccessStatusCode();
+    }
+}
---
+++ quotesapi.scraper/Program.cs
@@ -0,0 +1,29 @@
+﻿using HtmlAgilityPack;
+using quotesapi.scraper;
+
+const int pages = 2;
+
+for (int i = 1; i <= pages; i++) {
+    var html = $"https://www.azquotes.com/top_quotes.html?p={i}";
+    var web  = new HtmlWeb();
+
+    var api = new ApiService(new HttpClient());
+
+    await api.LoginAsync("admin", "admin");
+
+    var htmlDoc    = web.Load(html);
+    var quotesList = htmlDoc.DocumentNode.SelectSingleNode("//ul[contains(@class, 'list-quotes')]");
+    if (quotesList == null) {
+        throw new Exception("No quotes found on page");
+    }
+
+    var quotes = quotesList.SelectNodes("li");
+
+    Console.WriteLine($"Page {i}");
+    Console.WriteLine($"Found {quotes.Count} quotes");
+
+    foreach (var quote in quotes) {
+        var text = quote.SelectSingleNode(".//div/p/a[contains(@class, 'title')]").InnerText;
+        await api.AddQuoteAsync(text);
+    }
+}
